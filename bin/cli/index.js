31af4bab0a3cf2acf2575a5540d8120a00c4510d
borisding <boris.ding82@outlook.com>
2018-03-23 17:30:13 +0800

    boilerplate for avenue commands

--- bin/cli/index.js
+++ bin/cli/index.js
@@ -1,3 +1,33 @@
 #!/usr/bin/env node
 
 import sade from 'sade';
+import pkg from '@root/package';
+
+const program = sade('avenue');
+
+program.version(pkg.version);
+
+program
+  .command('new:controller <controller>')
+  .describe(
+    'Create new resource controller in `controllers` folder. Paired router will be created in `routers` folder by default.'
+  )
+  .option('-r, --router', 'Create new router for provided controller.', true)
+  .option(
+    '-m, --module',
+    'Name of sub-folder that controller resides in `controllers` folder.'
+  )
+  .action((controller, options) => {
+    console.log(controller);
+    console.log(options);
+  });
+
+program
+  .command('new:model <model>')
+  .describe('Create new model class in `models` folder.')
+  .action((model, options) => {
+    console.log(model);
+    console.log(options);
+  });
+
+program.parse(process.argv);
