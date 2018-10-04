8180d8e798588a873955171d56d9ac93e3fc9427
borisding <boris.ding82@outlook.com>
2018-10-04 15:59:18 +0800

    added scss build. script amendments

--- build/webpack/index.js
+++ build/webpack/index.js
@@ -1,39 +1,51 @@
 require('module-alias/register');
-
 const fs = require('fs');
 const path = require('path');
 const isDev = require('isdev');
+const autoprefixer = require('autoprefixer');
 const AssetsPlugin = require('assets-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const pkg = require('@root/package');
 const syspath = require('@config/syspath');
 
-const publicPath = '/js/';
+const publicPath = '/';
 
-// populate JS modules from `./src/assets/js` for multiple entries
-const getEntryFiles = () => {
+// populate respective module JS and SCSS files as entry points
+const getEntry = () => {
   const entryFiles = {};
   const jsPath = `${syspath.assets}/js`;
+  const scssPath = `${syspath.assets}/scss`;
 
   fs.readdirSync(jsPath).filter(file => {
     const { name, ext } = path.parse(file);
 
     if (ext === '.js') {
-      entryFiles[name] = `${jsPath}/${file}`;
+      entryFiles[name] = [`${jsPath}/${file}`];
+    }
+
+    // check if module has .scss file as well
+    // if there is, push as part of the module entry point
+    const moduleScss = `${scssPath}/${name}.scss`;
+
+    if (fs.existsSync(moduleScss)) {
+      entryFiles[name].push(moduleScss);
     }
   });
 
   return entryFiles;
 };
 
+// webpack's cofiguration
 const webpackConfig = {
   watch: isDev,
   mode: isDev ? 'development' : 'production',
   devtool: isDev ? 'cheap-module-inline-source-map' : 'source-map',
   context: syspath.src,
-  entry: getEntryFiles(),
+  entry: getEntry(),
   optimization: {
-    minimizer: [new UglifyJsPlugin()],
+    minimizer: [new UglifyJsPlugin(), new OptimizeCSSAssetsPlugin()],
     splitChunks: {
       cacheGroups: {
         vendor: {
@@ -46,9 +58,9 @@ const webpackConfig = {
   },
   output: {
     publicPath,
-    path: `${syspath.public}${publicPath}`,
-    filename: isDev ? '[name].js' : '[name].[contenthash].js',
-    chunkFilename: isDev ? '[id].js' : '[id].[contenthash].js'
+    path: syspath.public,
+    filename: isDev ? 'js/[name].js' : 'js/[name].[contenthash:8].js',
+    chunkFilename: isDev ? 'js/[id].js' : 'js/[id].[contenthash:8].js'
   },
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.css', '.scss'],
@@ -66,10 +78,33 @@ const webpackConfig = {
             cacheDirectory: !!isDev
           }
         }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 2, sourceMap: true }
+          },
+          {
+            loader: 'postcss-loader',
+            options: { plugins: () => [autoprefixer] }
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ]
       }
     ]
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: isDev ? 'css/[name].css' : 'css/[name].[contenthash:8].css',
+      chunkFilename: isDev ? 'css/[id].css' : 'css/[id].[contenthash:8].css'
+    }),
     new AssetsPlugin({
       prettyPrint: true,
       filename: 'bundles.json',
