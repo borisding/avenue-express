ccec9c3c3c1a4c2de22dfc602233c532749bbecc
borisding <boris.ding82@outlook.com>
2018-10-22 17:45:20 +0800

    config tweaks

--- config/index.js
+++ config/index.js
@@ -1,12 +1,19 @@
-const DEV = require('isdev');
-const SYSPATH = require('./syspath');
-const DB = require('./database');
 const ENV = require('./env-properties.json');
 
-// export combined configuration for `./src` usage as a whole
-module.exports = {
-  DEV,
-  DB,
-  ENV,
-  SYSPATH
-};
+// assigned with `process.env` or fallback to default port value
+// when one of the following is not defined in '.env' file
+if (!ENV['PORT']) {
+  ENV['PORT'] = +process.env.PORT || 3000;
+}
+
+// export built environment properties for app usage
+module.exports.ENV = ENV;
+
+// export named `DB` as part of config
+module.exports.DB = require('./database');
+
+// export named `SYSPATH` as part of config
+module.exports.SYSPATH = require('./syspath');
+
+// export named `DEV` as part of config
+module.exports.DEV = require('isdev');
