4f2ed7721ef5a7b0d2068ebec2b433338e8e4840
borisding <boris.ding82@outlook.com>
2018-10-23 21:50:22 +0800

    use `shallowMount` instead

--- tests/components/Home.vue.test.js
+++ tests/components/Home.vue.test.js
@@ -1,9 +1,9 @@
-import { shallow } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import Home from '@components/Home.vue';
 
 describe('Home.vue', () => {
   it('renders correctly', () => {
-    const wrapper = shallow(Home);
+    const wrapper = shallowMount(Home);
     expect(wrapper).toMatchSnapshot();
   });
 });
