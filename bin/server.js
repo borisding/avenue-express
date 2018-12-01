ac1e97d41876fc67780c2144f878da2f35cc1d85
borisding <boris.ding82@outlook.com>
2018-12-01 22:50:05 +0800

    tweak

--- bin/server.js
+++ bin/server.js
@@ -28,6 +28,8 @@ server.on('error', err => {
   }
 });
 
-process.on('SIGINT', () => {
-  process.exit(0);
+['SIGINT', 'SIGTERM'].forEach(signal => {
+  process.on(signal, () => {
+    server.close(() => process.exit(0));
+  });
 });
