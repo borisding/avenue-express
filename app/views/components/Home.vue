1ebab8347d78f61c1677f70b8c821a287728acaa
borisding <boris.ding82@outlook.com>
2020-03-31 15:22:28 +0800

    formatting

--- app/views/components/Home.vue
+++ app/views/components/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <article class="message is-info">
-  <div class="message-header">
-    <p>Greetings Widget</p>
-    <button class="delete" aria-label="delete"></button>
-  </div>
-  <div class="message-body"><span class="greetings">{{message}}</span></div>
-</article>
+    <div class="message-header">
+      <p>Greetings Widget</p>
+      <button class="delete" aria-label="delete"></button>
+    </div>
+    <div class="message-body">
+      <span class="greetings">{{message}}</span>
+    </div>
+  </article>
 </template>
 
 <script>
