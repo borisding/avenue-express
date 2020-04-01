00ef40fb6905e473a4cf390af44ed93ff026db30
borisding <boris.ding82@outlook.com>
2020-04-01 17:19:51 +0800

    change to async/await

--- bin/cli/utils.js
+++ bin/cli/utils.js
@@ -2,28 +2,24 @@ const fs = require('fs');
 const prettier = require('prettier');
 const { syspath } = require('@config');
 
-function prettyFormat(key, options) {
-  if (!options.name) {
-    return;
-  }
+async function prettyFormat(key, options) {
+  if (!options.name) return;
+  try {
+    const sequelizeConfig = require(`${syspath.root}/.sequelizerc`);
+    if (sequelizeConfig[key]) {
+      const filePath = `${sequelizeConfig[key]}/${options.name}.js`;
+      const text = fs.readFileSync(filePath, 'utf8');
 
-  const sequelizeConfig = require(`${syspath.root}/.sequelizerc`);
-  if (sequelizeConfig[key]) {
-    const filePath = `${sequelizeConfig[key]}/${options.name}.js`;
-    const text = fs.readFileSync(filePath, 'utf8');
-
-    prettier
-      .resolveConfig(filePath)
-      .then(options => {
-        const formatted = prettier.format(text, {
-          ...options,
-          parser: 'babel'
-        });
-        fs.writeFileSync(filePath, formatted, { encoding: 'utf8' });
-      })
-      .catch(error => {
-        console.log(error);
+      const prettierOptions = await prettier.resolveConfig(filePath);
+      const formatted = await prettier.format(text, {
+        ...prettierOptions,
+        parser: 'babel'
       });
+
+      fs.writeFileSync(filePath, formatted, { encoding: 'utf8' });
+    }
+  } catch (error) {
+    console.error(`Prettier failure: ${error}`);
   }
 }
 
