d1211e9a5d9ad2a2c07df387d88216f8318b4896
borisding <boris.ding82@outlook.com>
2020-04-01 10:59:47 +0800

    fixed typo. tweaks

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // make promises safe to avoid unhandled rejection
 require('make-promises-safe');
 
-// register module alises of porject directories
+// register module aliases of porject directories
 require('module-alias/register');
 
 // load environment variables at first place
 require('./env.loader');
 
 // start app server
-require('@bin/server');
+require('./bin/server');
