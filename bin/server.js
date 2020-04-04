75e5844d15dd95f3cb108974174ea50473511dd3
borisding <boris.ding82@outlook.com>
2020-04-04 18:51:37 +0800

    open app url for development mode. tweaks

--- bin/server.js
+++ bin/server.js
@@ -2,24 +2,30 @@
 
 const http = require('http');
 const app = require('@app');
+const { isDev } = require('@config');
 const { print } = require('@utils');
 
-const server = http.createServer(app);
 const PORT = parseInt(process.env.PORT, 10) || 3000;
-
-server.listen(PORT);
+const HOST = process.env.HOST || 'localhost';
+const server = http.createServer(app).listen(PORT, HOST);
 
 server.on('listening', () => {
   print.info(`App Server is up! Listening: ${PORT}`);
+  if (isDev) {
+    const url = `http://${HOST}:${PORT}`;
+    require('open')(url);
+  }
 });
 
 server.on('error', err => {
   switch (err.code) {
     case 'EACCES':
       print.error('Not enough privileges to run application server.', -1);
+      process.exit(1);
       break;
     case 'EADDRINUSE':
       print.error(`${PORT} is already in use.`, -1);
+      process.exit(1);
       break;
     default:
       throw err;
