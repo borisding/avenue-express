520a1adbeca959117bab98caa26ac2e15da5860a
borisding <boris.ding82@outlook.com>
2020-04-22 23:04:05 +0800

    add static images url that derived from assets folder

--- app/index.js
+++ app/index.js
@@ -54,6 +54,7 @@ app
   .use(express.json({ limit: '1mb' }))
   .use(express.urlencoded({ extended: true, limit: '10mb' }), hpp())
   .use(express.static(syspath.public))
+  .use('/static/images', express.static(`${syspath.assets}/images`))
   .get('/favicon.ico', (req, res) => res.status(204));
 
 app
