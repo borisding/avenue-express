74765a5e6cc18b53e02bc3ffe3e0354c419b4758
borisding <boris.ding82@outlook.com>
2021-01-23 20:52:18 +0800

    removed sequelize orm command

--- scripts/cli/index.js
+++ scripts/cli/index.js
@@ -3,7 +3,6 @@
 const sade = require('sade');
 const pkg = require('@root/package');
 const actions = require('./lib');
-const { CLI_ENV } = require('./utils');
 
 const program = sade('avenue').version(pkg.version);
 
@@ -33,26 +32,5 @@ program
   .describe('Generate new random key for application.')
   .option('-l, --length', 'Specify maximum length of generated random key.', 32)
   .action(actions.generateKey);
-
-program
-  .command('orm <command>')
-  .describe(
-    `Running sequelize-cli under the hood in ${CLI_ENV.DEV} environment.`
-  )
-  .action(actions.sequelizeCli(CLI_ENV.DEV));
-
-program
-  .command('orm:prod <command>')
-  .describe(
-    `Running sequelize-cli under the hood in ${CLI_ENV.PROD} environment.`
-  )
-  .action(actions.sequelizeCli(CLI_ENV.PROD));
-
-program
-  .command('orm:test <command>')
-  .describe(
-    `Running sequelize-cli under the hood in ${CLI_ENV.TEST} environment.`
-  )
-  .action(actions.sequelizeCli(CLI_ENV.TEST));
 
 program.parse(process.argv);
