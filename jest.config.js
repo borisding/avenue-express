38388dcb3203a6717598eb2fbfcad62a1b82b7aa
borisding <boris.ding82@outlook.com>
2021-01-27 20:21:02 +0800

    opt out sass extension

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/assets/mocks/fileMock.js',
-    '\\.(css|scss|sass)$': 'identity-obj-proxy'
+    '\\.(css|scss)$': 'identity-obj-proxy'
   },
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest'
